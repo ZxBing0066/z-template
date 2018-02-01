0bfb29de49bd288f7798352826bc18b2fae42811
ZxBing0066 <ZxBing0066@gmail.com>
2018-02-01 15:19:38 +0800

    add escape interpolate

--- index.js
+++ index.js
@@ -1,27 +1,82 @@
 var templateSettings = {
-    evaluate: /<%([\s\S]+?)%>/g,
     interpolate: /<%=([\s\S]+?)%>/g,
-    escape: /<%-([\s\S]+?)%>/g
+    escape: /<%-([\s\S]+?)%>/g,
+    evaluate: /<%([\s\S]+?)%>/g
 };
 
-var trim = function(str) {
-    if (typeof str === 'string') {
-        if (str.trim) return str.trim();
-        return str.replace(/^[\s]+|[\s]+$/g, '');
+var escapeMap = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#x27;',
+    '`': '&#x60;'
+};
+
+var getKeys = function(obj) {
+    var keys = [];
+    for (var key in obj) {
+        keys.push(key);
     }
-    return str;
+    return keys;
+};
+
+var escapeKeys = getKeys(escapeMap);
+var escapeRegExp = (function() {
+    var source = '(' + escapeKeys.join('|') + ')';
+    return RegExp(source, 'g');
+})();
+
+var escaper = function(str) {
+    return escapeMap[str];
+};
+
+var escape = function(str) {
+    return str.replace(escapeRegExp, escaper);
 };
 
 function template(str) {
     var source = 'var __t,__r;\n';
     source += 'with(data){\n__r = "';
-    str = str.replace(templateSettings.interpolate, function(match, expression) {
-        return '" + (' + expression + ') + "';
+
+    var matcher = RegExp(
+        [templateSettings.interpolate.source, templateSettings.escape.source, templateSettings.evaluate.source].join(
+            '|'
+        ),
+        'g'
+    );
+
+    str = str.replace(matcher, function(match, interpolate, escape, evaluate, index) {
+        if (interpolate) {
+            return '" + (' + interpolate + ') + "';
+        } else if (escape) {
+            return '" + __escape(' + escape + ') + "';
+        } else if (evaluate) {
+            return 'evaluate';
+        } else {
+            return '';
+        }
     });
     source += str;
     source += '"\n}\n';
     source += 'return __r;\n';
-    const render = new Function('data', source);
+    var handle = new Function('data', source);
+    var render = function(data) {
+        var cachedEscape, hasEscape;
+        if ('__escape' in data) {
+            cachedEscape = data.__escape;
+            data.__escape = escape;
+            hasEscape = true;
+        }
+        data.__escape = escape;
+        var result = handle(data);
+        if (hasEscape) {
+            data.__escape = cachedEscape;
+        } else {
+            delete data.__escape;
+        }
+        return result;
+    };
     return render;
 }
 
--- test/test.js
+++ test/test.js
@@ -9,9 +9,16 @@ const result = compile({
 });
 
 const result2 = template('<div><h1><%= title %></h1><p><%= content %></p></div>')({
-    title: 'hello world',
+    title: '<span>some html</span>hello world',
     content: 'this is the content'
 });
 
 console.log(compile, result);
 console.log(result2);
+
+const result3 = template('<div><h1><%- title %></h1><p><%- content %></p></div>')({
+    title: '<script>alert("xss")</script>hello',
+    content: '<script>alert("xss")</script>this is the content'
+});
+
+console.log(result3);
